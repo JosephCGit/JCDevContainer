a60e6ddf78f1e1b5666c8f221b8fb482a22f0370
JosephCGit <JosephCGit@users.noreply.github.com>
2024-03-26 19:05:44 +0000

    deploy: cc9c475b4ba88e5250de119a18408e25506a330f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "c069e05fb34e057acf0d0c2427ee4ebd",
-"/": "c069e05fb34e057acf0d0c2427ee4ebd",
+const RESOURCES = {"index.html": "4675bc8f6981e2d61ad2cde79ccf1b1b",
+"/": "4675bc8f6981e2d61ad2cde79ccf1b1b",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
